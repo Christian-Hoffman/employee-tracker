bd86376fada83491e9c791d01fb436ea8f818f9d
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-09-22 19:58:11 -0400

    intial setup for files and some database setup

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (department)
+VALUES  ('Sales'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal');
+
+INSERT INTO role ()
+VALUES  ('Sales Lead'),
+        ('Sales Associate'),
+        ('Senior Engineer'),
+        ('Junior Engineer'),
+        ('Accounting Manager'),
+        ('Accountant'),
+        ('Lawyer'),
+        ('Legal Associate');
+
+INSERT INTO role ()
+VALUES  (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        ();

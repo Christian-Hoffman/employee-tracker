8ddf8273fa47109d27b44ad2f6c1c40648f9c4c3
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-09-22 23:36:49 -0400

    created database setup and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,28 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department {
-
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
 };
 
 CREATE TABLE role {
-
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary Decimal(8,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 };
 
 CREATE TABLE employee {
-
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 };
--- db/seeds.sql
+++ db/seeds.sql
@@ -15,13 +15,14 @@ VALUES  ('Sales Lead'),
         ('Legal Associate');
 
 INSERT INTO role ()
-VALUES  (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        (),
-        ();
+VALUES  ("Rogue", "Amendiares", 130000, 1),
+        ("Panam", "Palmer", 90000, 1),
+        ("Viktor", "Vector", 180000, 2),
+        ("Judy", "Alvarez", 120000, 2),
+        ("Alt", "Cunningham", 120000, 2),
+        ("Johnny", "Silverhand", 130000, 3),
+        ("Adam", "Smasher", 100000, 3),
+        ("Goro", "Takemura", 210000, 4),
+        ("Jackie", "Wells", 160000, 4),
+        ("River", "Ward", 160000, 4);
+
